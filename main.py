503d31dcac5c8c6fee2b8128d553bf84371d4d4c
datavortex@gmail.com <datavortex@gmail.com@6d817a92-2925-b987-8c5f-e54ca84b80b4>
2011-01-21 23:01:18 +0000

    Party detail page finished

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 from appengine_utilities import sessions
 
+template.register_template_library('django.contrib.humanize.templatetags.humanize')
+
 DEBUGGING = True
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+ORDINALS = ('First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth')
 
 class Party(db.Model):
   name = db.StringProperty(required=True)
@@ -55,12 +58,22 @@ def GetUserFromSession():
 
 class PopulateTestData(RequestHandler):
   def get(self):
-    foo = Party(name='Test Party 1', secret='foo', email='datavortex+foo@gmail.com')
-    bar = Party(name='Test Party 2', secret='bar', email='datavortex+bar@gmail.com')
+    foo = Party(name='Test Party 1',
+                secret='foo',
+                email='datavortex+foo@gmail.com')
+    bar = Party(name='Test Party 2',
+                secret='bar',
+                email='datavortex+bar@gmail.com')
+    baz = Party(name='Test Party 3',
+                secret='baz',
+                email='datavortex+baz@gmail.com',
+                size=5,
+                people_names=['Person One','Person Two'])
     foo.put()
     bar.put()
+    baz.put()
     self.DEBUG('Created test data.')
-    
+
 
 class LandingWithoutKeyword(RequestHandler):
   """Initial Page that prompts for secret word. """
@@ -91,7 +104,7 @@ class SecretWord(RequestHandler):
       return
     if not matched:
       self.ERROR('Sorry, that does not appear to be a valid secret word.  Please try again.', 'get_keyword.html')
-      return    
+      return
     party = parties.get()
     sess = GetSession()
     sess['party_key_id'] = party.key().id()
@@ -103,8 +116,8 @@ class SecretWord(RequestHandler):
       else:
         template_vars['not_coming'] = True
     self.WriteTemplate('is_coming.html', template_vars)
-   
-class YesOrNo(RequestHandler):      
+
+class YesOrNo(RequestHandler):
    def post(self):
     """Handle yes and no responses."""
     coming = self.request.get('coming')
@@ -118,17 +131,25 @@ class YesOrNo(RequestHandler):
     if coming == 'yes':
       party.is_coming = True
       party.put()
-      template_vars = {'size': party.size, 'names': party.people_names}
+      template_vars = {'size': party.size,
+                       'names': party.people_names,
+                       '1to8': map(None, ORDINALS, party.people_names)}
       self.WriteTemplate('party_detail.html', template_vars)
       return
     self.ERROR('Please select either yes or no!', 'is_coming.html', {'name': party.name})
 
-class PartyDetails(RequestHandler):      
+class PartyDetails(RequestHandler):
    def post(self):
     """Get count of guests and guest names."""
-    count = self.request.get('count')
-    self.DEBUG('count: "%s"' % count)
     party = GetUserFromSession()
+    party.size = int(self.request.get('coming'))
+    names = [self.request.get('name%s' % n) for n in range(1, party.size+1)]
+    party.people_names = names
+    party.put()
+
+    self.DEBUG('Coming count: "%s"<br>Names are: %s' % (party.size, party.people_names))
+    return
+    # DISCARDABLE AFTER THIS LINE TO END OF FUNCTION
     if coming == 'no':
       party.is_coming = False
       party.put()
@@ -140,14 +161,14 @@ class PartyDetails(RequestHandler):
       self.DEBUG('Hooray!  See you there!')
       return
     self.ERROR('Please select either yes or no!', 'is_coming.html', {'name': party.name})
- 
-    
+
 
 def main():
     application = webapp.WSGIApplication([('/', LandingWithoutKeyword),
                                           ('/test', PopulateTestData),
                                           ('/secretword', SecretWord),
                                           ('/yesorno', YesOrNo),
+                                          ('/partydetail', PartyDetails),
                                           ('/.{1,10}', SecretWord)],
                                          debug=DEBUGGING)
     util.run_wsgi_app(application)
